1d5bc3aa3b676f555cec164e7e132aa33555642f
Shivam118 <sharmashushant118@gmail.com>
2022-03-08 15:02:35 +0530

    TitleRibbon component Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import Typography from "@mui/material/Typography";
 import LogoBar from './components/LogoBar';
+import TitleRibbon from './components/TitleRibbon';
 
 function App() {
   return (
     <div className="App">
       <LogoBar />
+      <TitleRibbon title="Proposals" bgColor="rgba(196,23,23,1)"/>
     </div>
   );
 }
