88ce92b9271bf4fe08da966048cdba34b98b5b72
Shubh Agrawal <shubhagrawal1280@gmail.com>
2022-03-08 22:23:34 +0530

    proponent form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
-import PropsalForm from "./components/ProposalForm";
+import ProposalForm from "./components/ProposalForm";
 import Home from "./components/Home";
+import ProponentForm from "./components/ProponentForm";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="pform" element={<PropsalForm />} />
+        <Route path="pform" element={<ProposalForm />} />
+        <Route path="aform" element={<ProponentForm />} />
       </Routes>
     </div>
   );
