519f13bfb8655dfc9c24556853c8639247d9d564
Shivam118 <sharmashushant118@gmail.com>
2022-03-11 12:34:39 +0530

    Bootstrap Folder Added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./components/AdminDashboard/src/store/index";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./bootstrap/dist/css/bootstrap.min.css";
 
 
 ReactDOM.render(
